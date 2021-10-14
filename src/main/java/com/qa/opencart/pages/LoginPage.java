8e6c582698f1b67ff17599e18dbf5e5b74dd60c1
Suman Agarwal <suman8626@gmail.com>
2021-10-14 16:15:51 +0530

    updates

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 
 import com.qa.opencart.utils.ElementUtil;
 
+import io.qameta.allure.Step;
+
 public class LoginPage {
 
 	private WebDriver driver;
@@ -27,24 +29,29 @@ public class LoginPage {
 
 	// 3. Page Actions/ methods/features-- will be called by testng have to be
 	// public in nature
+	
+	@Step("getting login page title....")
 	public String getLoginPageTitle() {
 		return driver.getTitle();
 	}
 
+	@Step("getting login page url....")
 	public String getLoginPageUrl() {
 		return driver.getCurrentUrl();
 	}
-
+	
+	@Step("checking Forgot Pwd Link Exist....")
 	public boolean isForgotPwdLinkExist() {
 		return elementUtil.doIsDisplayed(forgotPwdLink);
 
 	}
 
+	@Step("checking Register Link Exist....")
 	public boolean isRegisterLinkExist() {
 		return elementUtil.doIsDisplayed(registerLink);
 
 	}
-
+	@Step("login with username:{0} and password: {1}")
 	public AccountsPage doLogin(String un, String pwd) {
 		elementUtil.doSendKeys(emailID, un);
 		elementUtil.doSendKeys(password, pwd);
@@ -52,6 +59,7 @@ public class LoginPage {
 		return new AccountsPage(driver); // returning the object of account page
 	}
 
+	@Step("navigating to register page....")
 	public RegisterationPage navigateToRegisterPage() {
 		elementUtil.doClick(registerLink);
 		return new RegisterationPage(driver);
--- src/test/java/com/qa/opencart/test/LoginPageTest.java
+++ src/test/java/com/qa/opencart/test/LoginPageTest.java
@@ -6,8 +6,18 @@ import org.testng.annotations.Test;
 import com.qa.opencart.base.BaseTest;
 import com.qa.opencart.utils.Constants;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+
+@Epic("Epic 100: Open Cart App- Design login page")
+@Story("US 101 : Login page features with some basic modules......")
 public class LoginPageTest extends BaseTest {
 	
+	@Description("login page title test")
+	@Severity(SeverityLevel.NORMAL)
 	@Test
 	public void loginPageTitleTest() {
 		String title = loginPage.getLoginPageTitle();
@@ -15,6 +25,8 @@ public class LoginPageTest extends BaseTest {
 		Assert.assertEquals(title, Constants.LOGIN_PAGE_TITLE);
 	
 	}
+	@Description("login page url test")
+	@Severity(SeverityLevel.NORMAL)
 	@Test
 	public void loginPageUrlTest() {
 		String url = loginPage.getLoginPageUrl();
@@ -24,16 +36,22 @@ public class LoginPageTest extends BaseTest {
 	
 	}
 	
+	@Description("login page forgotpwd link test")
+	@Severity(SeverityLevel.CRITICAL)
 	@Test
 	public void forgotPwdLinkTest() {
 		Assert.assertTrue(loginPage.isForgotPwdLinkExist());
 	}
 	
+	@Description("login page register link test")
+	@Severity(SeverityLevel.CRITICAL)
 	@Test
 	public void registerLinkTest() {
 		Assert.assertTrue(loginPage.isRegisterLinkExist());
 	}
 	
+	@Description("login page  test")
+	@Severity(SeverityLevel.BLOCKER)
 	@Test
 	public void loginTest() {
 		//trim is used to remove spaces
